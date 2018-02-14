87c17b2cb28d1ab4315acd820ff678d17406153c
Bagas Udi Sahsangka <udibagas@gmail.com>
2018-02-14 15:08:18 +0700

    delete extra spaces

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@
 
 Auth::routes();
 
-
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/', 'HomeController@index')->name('home');
     Route::get('/home', 'HomeController@index')->name('home');
