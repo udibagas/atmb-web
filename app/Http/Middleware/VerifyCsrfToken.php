6cf4080265e0c61609d274d20fc887e987e6be61
Bagas Udi Sahsangka <udibagas@gmail.com>
2018-02-14 18:04:40 +0700

    test

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Closure;
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -14,6 +15,20 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    // public function handle($request, Closure $next)
+    // {
+    //     if (
+    //         $this->isReading($request) ||
+    //         $this->runningUnitTests() ||
+    //         $this->inExceptArray($request) ||
+    //         $this->tokensMatch($request)
+    //     ) {
+    //         return $this->addCookieToResponse($request, $next($request));
+    //     }
+    //
+    //     throw new TokenMismatchException;
+    // }
 
     public function handle($request, Closure $next)
     {
