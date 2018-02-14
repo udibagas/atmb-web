e65b47297411a68223b06a38a9aa2d7edb3e73eb
Bagas Udi Sahsangka <udibagas@gmail.com>
2018-02-14 19:40:12 +0700

    add meta

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Login</title>
 
